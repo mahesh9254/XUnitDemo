519ce06ef386e3c28ebf0caf63d281a98cf8e597
Mahesh Kumar <MKumar@CheckAlt-Eras.com>
2021-11-15 15:13:03 +0530

    Add project files.

---
+++ XUnitDemo/Interfaces/IShoppingCartService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XUnitDemo.Entities;
+
+namespace XUnitDemo.Interfaces
+{
+   public interface IShoppingCartService
+    {
+        IEnumerable<ShoppingItem> GetAllItems();
+        ShoppingItem Add(ShoppingItem newItem);
+        ShoppingItem GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
---
+++ XUnitDemo/Services/ShoppingCartService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XUnitDemo.Entities;
+using XUnitDemo.Interfaces;
+
+namespace XUnitDemo.Services
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        public ShoppingItem Add(ShoppingItem newItem) => throw new NotImplementedException();
+        public IEnumerable<ShoppingItem> GetAllItems() => throw new NotImplementedException();
+        public ShoppingItem GetById(Guid id) => throw new NotImplementedException();
+        public void Remove(Guid id) => throw new NotImplementedException();
+    }
+}
